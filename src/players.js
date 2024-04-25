03670d950d83a1be85ee65217c46d4035ed2e46f
azizoo12 <mohamedlbaze@gmail.com>
2024-04-25 14:43:02 +0100

    reactProps

---
+++ src/players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Paris Saint-Germain",
+      nationality: "Argentine",
+      jerseyNumber: 30,
+      age: 34,
+      imageUrl: "https://via.placeholder.com/150"
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Manchester United",
+      nationality: "Portuguese",
+      jerseyNumber: 7,
+      age: 37,
+      imageUrl: "https://via.placeholder.com/150"
+    },
+    {
+      name: "Neymar Jr.",
+      team: "Paris Saint-Germain",
+      nationality: "Brazilian",
+      jerseyNumber: 10,
+      age: 30,
+      imageUrl: "https://via.placeholder.com/150"
+    },
+    {
+      name: "Mohamed Salah",
+      team: "Liverpool",
+      nationality: "Egyptian",
+      jerseyNumber: 11,
+      age: 29,
+      imageUrl: "https://via.placeholder.com/150"
+    }
+  ];
+  
+  export default players;
+  
